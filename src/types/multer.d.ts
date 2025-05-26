382a22ae3df801fb3fd984ea7a797ceb932ffe04
Muhammed Murad <129909845+mpro775@users.noreply.github.com>
2025-05-26 20:05:34 +0300

    Update multer.d.ts

--- src/types/multer.d.ts
+++ src/types/multer.d.ts
@@ -1,1 +1,20 @@
-declare module 'multer';
+// types/multer.d.ts
+declare module 'multer' {
+  import { Request } from 'express';
+
+  namespace multer {
+    interface File {
+      /** مثال على بعض الحقول الشائعة */
+      fieldname: string;
+      originalname: string;
+      encoding: string;
+      mimetype: string;
+      buffer: Buffer;
+      size: number;
+    }
+  }
+
+  function multer(options?: multer.Options): any;
+  namespace multer {}
+  export = multer;
+}
